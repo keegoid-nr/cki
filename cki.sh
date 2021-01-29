35e9867aa770a9ab451064c506bc505979802fda
Keegan Mullaney <kmullaney@newrelic.com>
2021-01-28 21:32:50 -0800

    improve code readability

--- cki.sh
+++ cki.sh
@@ -6,17 +6,53 @@ cki_has() {
   type "$1" > /dev/null 2>&1
 }
 
-cki_go() {
-  local nspace
-  local cluster-info
-  local cluster-resources
-  local ns-resources
+cki_echo() {
+  echo
+  echo "~~ ${1} ~~"
+  echo
+}
 
+
+cki_go() {
   if cki_has kubectl; then
-    read -rep "Enter your namespace   : " -i "" nspace
-    read -rep "Enter cluster resources to describe   : " -i "ns svc sc node pv" cluster-resources
-    read -rep "Enter namespace resources to describe   : " -i "role rolebinding service sa sts pvc pod" ns-resources
-    info=('version --short' 'get cs -A' 'get netpol -A -o wide' 'api-versions' 'api-resources -o wide' 'get apiservices.apiregistration.k8s.io'); for r in "${info[@]}"; do echo; echo "~~ $r ~~"; echo; kubectl $r; done >k8s-info.log 2>&1; for r in $cluster-resources; do echo; echo "~~ $r ~~"; echo; kubectl describe $(kubectl get $r -o name --no-headers=true); done >k8s-resources.yaml 2>&1; for r in $ns-resources; do echo; echo "~~ $r ~~"; echo; kubectl describe $(kubectl get $r -o name -n $nspace) -n $nspace; done >>k8s-resources.yaml 2>&1
+    local nspace
+    local cluster-info
+    local cluster-resources
+    local ns-resources
+
+    cluster-info=('version --short' 'get cs -A' 'get netpol -A -o wide' 'api-versions' 'api-resources -o wide' 'get apiservices.apiregistration.k8s.io')
+
+    read -rep "Enter your namespace   : " nspace
+    read -rep "Enter cluster resources to describe   : " -i 'ns svc sc node pv' cluster-resources
+    read -rep "Enter namespace resources to describe   : " -i 'role rolebinding service sa sts pvc pod' ns-resources
+
+    # cluster info
+    for r in "${info[@]}"
+    do
+      cki_echo "${r}"
+      kubectl "${r}"
+    done
+
+    >k8s-info.log 2>&1
+
+    # cluster resources
+    for r in $cluster-resources
+    do
+      cki_echo "${r}"
+      kubectl describe $(kubectl get "${r}" -o name --no-headers=true)
+    done
+
+    >k8s-resources.yaml 2>&1
+
+    # namespace specific resources
+    for r in $ns-resources
+    do
+      cki_echo "${r}"
+      kubectl describe $(kubectl get "${r}" -o name -n $nspace) -n $nspace
+    done
+
+    >>k8s-resources.yaml 2>&1
+
   else
     echo >&2 'You need kubectl to run this script.'
     exit 1
@@ -26,7 +62,7 @@ cki_go() {
 }
 
 cki_reset() {
-  unset -f cki_go
+  unset -f cki_has cki_echo cki_go
 }
 
 cki_go
