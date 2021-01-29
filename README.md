33ad18973524255a4e42bd9e65cf2f4f1419d889
Keegan Mullaney <kmullaney@newrelic.com>
2021-01-28 20:25:40 -0800

    prompt for ns

--- README.md
+++ README.md
@@ -7,13 +7,11 @@ Assist in troubleshooting Kubernetes clusters.
 The following cURL or Wget commands will download and run a script which will collect cluster info, resources at the cluster level, and resources from within the specified namespace.
 
 ```
-NSPACE=<SET_YOUR_NAMESPACE_HERE>
-curl -o- https://raw.githubusercontent.com/keegoid-nr/cki/v0.4/cki.sh | bash
+curl -o- https://raw.githubusercontent.com/keegoid-nr/cki/v0.5/cki.sh | bash
 ```
 
 ```
-NSPACE=<SET_YOUR_NAMESPACE_HERE>
-wget -qO- https://raw.githubusercontent.com/keegoid-nr/cki/v0.4/cki.sh | bash
+wget -qO- https://raw.githubusercontent.com/keegoid-nr/cki/v0.5/cki.sh | bash
 ```
 
 Any resources your Kubernetes user does not have access to will be skipped.
--- cki.sh
+++ cki.sh
@@ -7,7 +7,10 @@ cki_has() {
 }
 
 cki_go() {
+  local NSPACE
+
   if cki_has kubectl; then
+    read -rep "Enter your namespace   : " NSPACE
     info=('version --short' 'get cs -A' 'get netpol -A -o wide' 'api-versions' 'api-resources -o wide' 'get apiservices.apiregistration.k8s.io'); for r in "${info[@]}"; do echo; echo "~~ $r ~~"; echo; kubectl $r; done >k8s-info.log 2>&1; resources="ns svc sc node pv"; for r in $resources; do echo; echo "~~ $r ~~"; echo; kubectl describe $(kubectl get $r -o name --no-headers=true); done >k8s-resources.yaml 2>&1; resources="role rolebinding service sa sts pvc pod"; for r in $resources; do echo; echo "~~ $r ~~"; echo; kubectl describe $(kubectl get $r -o name -n $NSPACE) -n $NSPACE; done >>k8s-resources.yaml 2>&1
   else
     echo >&2 'You need kubectl to run this script.'
